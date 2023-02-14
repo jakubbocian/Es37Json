b5d70edcbb37409077788899dc32693d9bee29cf
ZTDavide <59927455+ZTDavide@users.noreply.github.com>
2023-02-14 10:27:10 +0100

    committo due righe

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,28 @@
 package org.example;
 
+import com.google.gson.Gson;
+
+import java.io.IOException;
+import java.nio.file.*;
+import java.util.ArrayList;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    private static String readFromFile(String path) throws IOException {
+        byte[] content = Files.readAllBytes(Paths.get(path));
+        return new String(content);
     }
+
+    public static void main(String[] args) throws IOException {
+        Gson gson = new Gson();
+        String json = readFromFile("src/main/resources/analisi.json");
+        String json2 = readFromFile("src/main/resources/verifica.json");
+        Analisi[] analisi = gson.fromJson(json, Analisi[].class);
+        Verifica[] verifica = gson.fromJson(json2, Verifica[].class);
+
+        
+
+
+    }
+
+
 }
