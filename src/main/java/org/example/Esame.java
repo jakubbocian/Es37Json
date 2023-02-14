2947bf99618f45c1a8cadb082a15cc8ddee76b65
jakubbocian <jakub.bocian@mpolo.it>
2023-02-14 10:39:47 +0100

    Creazione classi per gli oggetti json

---
+++ src/main/java/org/example/Esame.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import java.util.Date;
+
+public class Esame {
+    Date dataOraAnalisi;
+    String codiceOperatore;
+    String matricolaStrumenti[];
+    String nomeAnalisi;
+    Risultato risultato;
+}
---
+++ src/main/java/org/example/EsameRegistrato.java
@@ -0,0 +1,9 @@
+package org.example;
+
+import java.lang.reflect.Array;
+
+public class EsameRegistrato {
+    String nome;
+    int giorniDopoPrelievo;
+    Strumenti strumenti[];
+}
